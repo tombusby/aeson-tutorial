1ad424268601efa9c9d37f12264d5f3312c54c89
Tom Busby <tom@busby.ninja>
2018-11-14 09:30:56 +0100

    Decode to tuples for mixed-type lists

--- 01-Basic-Encode-Decode.hs
+++ 01-Basic-Encode-Decode.hs
@@ -13,6 +13,10 @@ main = do
     putStr2Lns (eitherDecode "\"foo\"" :: Either String Integer)
     putStrLn "eitherDecode \"foo\" :: Either String String"
     putStr2Lns (eitherDecode "foo" :: Either String String)
+    putStrLn "eitherDecode \"[1,2,[3,true]]\" :: Either String (Int, Int, (Int, Bool))"
+    putStr2Lns (eitherDecode "[1,2,[3,true]]" :: Either String (Int, Int, (Int, Bool)))
+    putStrLn "eitherDecode \"[1,2,[3,4]]\" :: Either String (Int, Int, (Int, Bool))"
+    putStr2Lns (eitherDecode "[1,2,[3,4]]" :: Either String (Int, Int, (Int, Bool)))
     where
         putStr2Lns :: Show a => a -> IO ()
         putStr2Lns = putStrLn . (++"\n") . show
